b4a286794525d765991c1cc7003f747907dae398
max <maxjyass@gmail.com>
2025-10-19 14:30:03 -0500

    add manual bloodhound install

--- startup.sh
+++ startup.sh
@@ -72,3 +72,11 @@ tar -xvzf bloodhound-cli-linux-amd64.tar.gz
 # Install bloodhound ce via the Bloodhound CLI
 ./bloodhound-cli install
 
+# Alternative manual installation method (if needed) 
+# git clone https://github.com/SpecterOps/BloodHound.git
+# cd BloodHound/examples/docker-compose
+# cp docker-compose.yml docker-compose.bak
+# cp .env.example .env
+# sed -i 's/BLOODHOUND_HOST=127.0.0.1/BLOODHOUND_HOST=0.0.0.0/' .env
+# sudo docker compose up 
+
